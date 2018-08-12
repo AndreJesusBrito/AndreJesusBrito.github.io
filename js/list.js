32603a82fc7a8f2ab478d2a7856d8e69265762d1
AndreJesusBrito <35274086+AndreJesusBrito@users.noreply.github.com>
2018-08-12 23:03:27 +0100

    Update

---
+++ js/list.js
@@ -0,0 +1,18 @@
+const FADETIME = 100;
+
+document.querySelectorAll(".list").forEach(element => {
+    element.children[0].addEventListener("click", event => {
+        const list = event.path[1].children[1].children;
+        const deltaTime = FADETIME / list.length;
+        const state = list[0].style.display === "" ? "none" : "";
+
+        list[0].style.display = state;
+        for(let i = 1; i < list.length; i++) {
+            setTimeout(() => {
+                list[i].style.display = state;
+            }, deltaTime*i);
+        }
+
+        event.stopPropagation();
+    });
+});
