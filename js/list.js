4a4dd8b7e1a461f1fae35ec8d119f5a289a3b25c
AndreJesusBrito <35274086+AndreJesusBrito@users.noreply.github.com>
2018-09-12 23:59:15 +0100

    More Updates :D

--- js/list.js
+++ js/list.js
@@ -4,7 +4,7 @@ document.querySelectorAll(".list").forEach(element => {
     element.children[0].addEventListener("click", event => {
         const list = event.path[1].children[1].children;
         const deltaTime = FADETIME / list.length;
-        const state = list[0].style.display === "" ? "none" : "";
+        const state = list[0].style.display === "none" ? "" : "none";
 
         list[0].style.display = state;
         for(let i = 1; i < list.length; i++) {
